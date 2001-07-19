3a6af15176de3842bcee5c4aa337836e4245e484
Michael Bayne <mdb@threerings.net>
2001-07-19 07:48:58 +0000

    Whoops. Fergot return value.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@72 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- tests/src/java/com/threerings/presents/server/TestProvider.java
+++ tests/src/java/com/threerings/presents/server/TestProvider.java
@@ -1,5 +1,5 @@
 //
-// $Id: TestProvider.java,v 1.1 2001/07/19 07:48:25 mdb Exp $
+// $Id: TestProvider.java,v 1.2 2001/07/19 07:48:58 mdb Exp $
 
 package com.threerings.cocktail.cher.server.test;
 
@@ -13,5 +13,6 @@ public class TestProvider
     public Object[] handleTestRequest (String one, int two)
     {
         Log.info("Test request [one=" + one + ", two=" + two + "].");
+        return null;
     }
 }
