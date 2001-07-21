398362623be24302edb56660e31facb3aa3e69fd
Michael Bayne <mdb@threerings.net>
2001-07-21 01:06:24 +0000

    Added comments that explain the temporal validity constraints on the objects available via the context.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@98 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/util/PresentsContext.java
+++ src/java/com/threerings/presents/util/PresentsContext.java
@@ -1,21 +1,24 @@
 //
-// $Id: PresentsContext.java,v 1.2 2001/07/20 23:23:50 mdb Exp $
+// $Id: PresentsContext.java,v 1.3 2001/07/21 01:06:24 mdb Exp $
 
 package com.threerings.cocktail.cher.util;
 
 import com.samskivert.util.Context;
-import com.threerings.cocktail.cher.dobj.DObjectManager;
+
 import com.threerings.cocktail.cher.client.Client;
+import com.threerings.cocktail.cher.dobj.DObjectManager;
 
 public interface CherContext extends Context
 {
     /**
-     * Returns a reference to the client.
+     * Returns a reference to the client. This reference should be valid
+     * for the life of the application.
      */
     public Client getClient ();
 
     /**
-     * Returns a reference to the distributed object manager.
+     * Returns a reference to the distributed object manager. This
+     * reference is only valid for the duration of a session.
      */
     public DObjectManager getDObjectManager ();
 }
