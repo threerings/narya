b4d5f76c700fceee7a73fb9c2a2c6f6283ef0a26
Walter Korman <shaper@threerings.net>
2001-07-23 22:47:03 +0000

    Fixed to conform to the updated SceneParser interface.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@115 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/miso/client/XMLSceneParser.java
+++ src/java/com/threerings/miso/client/XMLSceneParser.java
@@ -1,15 +1,14 @@
 //
-// $Id: XMLSceneParser.java,v 1.1 2001/07/23 22:45:04 shaper Exp $
+// $Id: XMLSceneParser.java,v 1.2 2001/07/23 22:47:03 shaper Exp $
 
 package com.threerings.miso.scene;
 
-import java.io.InputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class XMLSceneParser implements SceneParser
 {
-    public ArrayList loadScenes (InputStream tis) throws IOException
+    public ArrayList loadScenes (String fname) throws IOException
     {
         // TBD
         return null;
