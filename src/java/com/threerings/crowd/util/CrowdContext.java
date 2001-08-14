0da19f558ec7a9b960db6e1e23a35725595cdfb9
Michael Bayne <mdb@threerings.net>
2001-08-14 03:22:19 +0000

    Documented class.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@234 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/crowd/util/CrowdContext.java
+++ src/java/com/threerings/crowd/util/CrowdContext.java
@@ -1,11 +1,15 @@
 //
-// $Id: CrowdContext.java,v 1.1 2001/07/20 20:07:38 mdb Exp $
+// $Id: CrowdContext.java,v 1.2 2001/08/14 03:22:19 mdb Exp $
 
 package com.threerings.cocktail.party.util;
 
 import com.threerings.cocktail.cher.util.CherContext;
 import com.threerings.cocktail.party.client.LocationManager;
 
+/**
+ * The party context provides access to the various managers, etc. that
+ * are needed by the party client code.
+ */
 public interface PartyContext extends CherContext
 {
     /**
