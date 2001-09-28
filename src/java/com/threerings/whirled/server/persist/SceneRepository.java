c7dfb31972e941bc3de7bd171280e814935fa581
Michael Bayne <mdb@threerings.net>
2001-09-28 22:15:57 +0000

    Added documentation.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@358 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/whirled/server/persist/SceneRepository.java
+++ src/java/com/threerings/whirled/server/persist/SceneRepository.java
@@ -1,12 +1,20 @@
 //
-// $Id: SceneRepository.java,v 1.3 2001/09/21 03:15:02 mdb Exp $
+// $Id: SceneRepository.java,v 1.4 2001/09/28 22:15:57 mdb Exp $
 
 package com.threerings.whirled.server.persist;
 
 import com.samskivert.io.PersistenceException;
+
 import com.threerings.whirled.data.Scene;
 import com.threerings.whirled.util.NoSuchSceneException;
 
+/**
+ * The scene repository provides the basic interface for loading and
+ * updating scenes. It is used by the scene registry and though more scene
+ * related persistence services may be needed in a full-fledged
+ * application, the scene repository only encapsulates those needed by the
+ * scene registry and other services provided by the Whirled framework.
+ */
 public interface SceneRepository
 {
     /**
