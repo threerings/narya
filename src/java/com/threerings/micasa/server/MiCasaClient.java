758bac6f289a90f6f15139fcaa700a4c04bfcad3
Michael Bayne <mdb@threerings.net>
2001-10-09 19:23:50 +0000

    Extend PartyClient, do the right thing with our BootstrapData.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@419 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/micasa/server/MiCasaClient.java
+++ src/java/com/threerings/micasa/server/MiCasaClient.java
@@ -1,26 +1,32 @@
 //
-// $Id: MiCasaClient.java,v 1.1 2001/10/09 18:20:08 mdb Exp $
+// $Id: MiCasaClient.java,v 1.2 2001/10/09 19:23:50 mdb Exp $
 
 package com.threerings.micasa.server;
 
 import com.threerings.cocktail.cher.net.BootstrapData;
-import com.threerings.cocktail.cher.server.CherClient;
+import com.threerings.cocktail.party.server.PartyClient;
+
+import com.threerings.micasa.data.MiCasaBootstrapData;
 
 /**
  * Extends the cher client and provides bootstrap data specific to the
  * MiCasa services.
  */
-public class MiCasaClient extends CherClient
+public class MiCasaClient extends PartyClient
 {
     // documentation inherited
     protected BootstrapData createBootstrapData ()
     {
-        return new BootstrapData();
+        return new MiCasaBootstrapData();
     }
 
     // documentation inherited
     protected void populateBootstrapData (BootstrapData data)
     {
         super.populateBootstrapData(data);
+
+        // let the client know their default lobby oid
+        ((MiCasaBootstrapData)data).defLobbyOid =
+            MiCasaServer.lobreg.getDefaultLobbyOid();
     }
 }
