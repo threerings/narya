33b76cd711b1ebb83ae6977f09f0449e8fe740f0
Michael Bayne <mdb@threerings.net>
2001-10-19 18:03:06 +0000

    Added standard error code for internal server errors.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@504 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/client/InvocationCodes.java
+++ src/java/com/threerings/presents/client/InvocationCodes.java
@@ -1,5 +1,5 @@
 //
-// $Id: InvocationCodes.java,v 1.2 2001/10/11 04:07:52 mdb Exp $
+// $Id: InvocationCodes.java,v 1.3 2001/10/19 18:03:06 mdb Exp $
 
 package com.threerings.presents.client;
 
@@ -17,4 +17,10 @@ public interface InvocationCodes
      * or a string code explaining the reason for failure.
      */
     public static final String SUCCESS = "success";
+
+    /** An error code returned to clients when a service cannot be
+     * performed because of some internal server error that we couldn't
+     * explain in any meaningful way (things like null pointer
+     * exceptions). */
+    public static final String INTERNAL_ERROR = "m.internal_error";
 }
