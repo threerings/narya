ea3964d7026c913243b9034ef0eb75ae338cbc30
Michael Bayne <mdb@threerings.net>
2001-10-23 23:47:33 +0000

    Now uses TableManager to do table management.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@533 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/micasa/lobby/table/TableLobbyManager.java
+++ src/java/com/threerings/micasa/lobby/table/TableLobbyManager.java
@@ -1,19 +1,41 @@
 //
-// $Id: TableLobbyManager.java,v 1.1 2001/10/23 20:24:10 mdb Exp $
+// $Id: TableLobbyManager.java,v 1.2 2001/10/23 23:47:33 mdb Exp $
 
 package com.threerings.micasa.lobby.table;
 
+import com.threerings.parlor.server.TableManager;
+import com.threerings.parlor.server.TableManagerProvider;
 import com.threerings.micasa.lobby.LobbyManager;
 
 /**
  * Extends lobby manager only to ensure that a table lobby object is used
  * for table lobbies.
  */
-public class TableLobbyManager extends LobbyManager
+public class TableLobbyManager
+    extends LobbyManager implements TableManagerProvider
 {
+    // documentation inherited
+    protected void didStartup ()
+    {
+        super.didStartup();
+
+        // now that we have our place object, we can create our table
+        // manager
+        _tmgr = new TableManager(this);
+    }
+
     // documentation inherited
     protected Class getPlaceObjectClass ()
     {
         return TableLobbyObject.class;
     }
+
+    // documentation inherited
+    public TableManager getTableManager ()
+    {
+        return _tmgr;
+    }
+
+    /** A reference to our table manager. */
+    protected TableManager _tmgr;
 }
