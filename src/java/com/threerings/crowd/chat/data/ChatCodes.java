e6003b2b3d5f1477672d203a371565fe5aead9fc
Michael Bayne <mdb@threerings.net>
2002-02-06 22:47:16 +0000

    Fixed spelling error.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@946 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/crowd/chat/data/ChatCodes.java
+++ src/java/com/threerings/crowd/chat/data/ChatCodes.java
@@ -1,5 +1,5 @@
 //
-// $Id: ChatCodes.java,v 1.4 2001/12/16 21:46:46 mdb Exp $
+// $Id: ChatCodes.java,v 1.5 2002/02/06 22:47:16 mdb Exp $
 
 package com.threerings.crowd.chat;
 
@@ -17,7 +17,7 @@ public interface ChatCodes extends InvocationCodes
      * currently occupied by the client. This is the only type of chat
      * message that will be delivered unless the chat director is
      * explicitly provided with other chat message sources via {@link
-     * ChatDirector#addAuxilliarySource}. */
+     * ChatDirector#addAuxiliarySource}. */
     public static final String PLACE_CHAT_TYPE = "placeChat";
 
     /** The message identifier for a speak request message. */
