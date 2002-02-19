b01df00f3f471bbe3a8164e5ff9b6b888dd95749
Michael Bayne <mdb@threerings.net>
2002-02-19 07:21:33 +0000

    Use a border layout.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@1040 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- tests/src/java/com/threerings/miso/client/ScrollingFrame.java
+++ tests/src/java/com/threerings/miso/client/ScrollingFrame.java
@@ -1,16 +1,14 @@
 //
-// $Id: ScrollingFrame.java,v 1.1 2002/02/17 23:48:37 mdb Exp $
+// $Id: ScrollingFrame.java,v 1.2 2002/02/19 07:21:33 mdb Exp $
 
 package com.threerings.miso.scene;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GraphicsConfiguration;
 
 import javax.swing.JFrame;
-
-import com.samskivert.swing.GroupLayout;
-import com.samskivert.swing.VGroupLayout;
 
 /**
  * The main application window.
@@ -31,12 +29,6 @@ public class ScrollingFrame extends JFrame
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // center the scene view within the frame
-        GroupLayout gl = new VGroupLayout();
-        gl.setJustification(GroupLayout.CENTER);
-        gl.setOffAxisJustification(GroupLayout.CENTER);
-        getContentPane().setLayout(gl);
-
         // set the frame and content panel background to black
         setBackground(Color.black);
         getContentPane().setBackground(Color.black);
@@ -54,7 +46,7 @@ public class ScrollingFrame extends JFrame
 
         // now add the new one
         _panel = panel;
-	getContentPane().add(_panel);
+	getContentPane().add(_panel, BorderLayout.CENTER);
     }
 
     protected Component _panel;
