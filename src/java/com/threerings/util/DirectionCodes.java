de0a6b70a859eaa0ee4052da170144cc7778eb5f
Walter Korman <shaper@threerings.net>
2002-03-26 01:31:15 +0000

    Added codes for left/right, clockwise/counter-clockwise, and horizontal/vertical.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@1148 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/util/DirectionCodes.java
+++ src/java/com/threerings/util/DirectionCodes.java
@@ -1,5 +1,5 @@
 //
-// $Id: DirectionCodes.java,v 1.1 2001/12/17 03:32:32 mdb Exp $
+// $Id: DirectionCodes.java,v 1.2 2002/03/26 01:31:15 shaper Exp $
 
 package com.threerings.util;
 
@@ -11,6 +11,24 @@ public interface DirectionCodes
 {
     /** A direction code indicating no direction. */
     public static final int NONE = -1;
+
+    /** A direction code indicating moving left. */
+    public static final int LEFT = 0;
+
+    /** A direction code indicating moving right. */
+    public static final int RIGHT = 1;
+
+    /** A direction code indicating a counter-clockwise rotation. */
+    public static final int CCW = 0;
+
+    /** A direction code indicating a clockwise rotation. */
+    public static final int CW = 1;
+
+    /** A direction code indicating horizontal movement. */
+    public static final int HORIZONTAL = 0;
+
+    /** A direction code indicating vertical movement. */
+    public static final int VERTICAL = 1;
 
     /** A direction code indicating southwest. */
     public static final int SOUTHWEST = 0;
