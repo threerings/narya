bf239bde957721602cf1e1440b45ffbfed9cacaa
Michael Bayne <mdb@threerings.net>
2002-09-17 01:23:09 +0000

    Update the username in the body object when it is changed in the client.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@1686 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/crowd/server/CrowdClient.java
+++ src/java/com/threerings/crowd/server/CrowdClient.java
@@ -1,9 +1,11 @@
 //
-// $Id: CrowdClient.java,v 1.8 2002/03/05 05:33:25 mdb Exp $
+// $Id: CrowdClient.java,v 1.9 2002/09/17 01:23:09 mdb Exp $
 
 package com.threerings.crowd.server;
 
 import com.threerings.presents.server.PresentsClient;
+
+import com.threerings.crowd.data.BodyObject;
 
 /**
  * The crowd client extends the presents client but doesn't really do
@@ -13,4 +15,12 @@ import com.threerings.presents.server.PresentsClient;
  */
 public class CrowdClient extends PresentsClient
 {
+    // documentation inherited
+    public void setUsername (String username)
+    {
+        super.setUsername(username);
+
+        // update the username in our body object
+        ((BodyObject)_clobj).setUsername(username);
+    }
 }
