af5b530b4cafe9c9bb42d9b9a763b84cc0354ede
Walter Korman <shaper@threerings.net>
2002-10-29 23:44:23 +0000

    Fixed log message formatting.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@1859 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/media/RegionManager.java
+++ src/java/com/threerings/media/RegionManager.java
@@ -1,5 +1,5 @@
 //
-// $Id: RegionManager.java,v 1.5 2002/10/29 20:33:26 shaper Exp $
+// $Id: RegionManager.java,v 1.6 2002/10/29 23:44:23 shaper Exp $
 
 package com.threerings.media;
 
@@ -49,8 +49,8 @@ public class RegionManager
     {
         // sanity-check the rectangle
         if (rect == null || (rect.width <= 0 || rect.height <= 0)) {
-            Log.warning("Attempt to add invalid rectangle as a dirty region?!" +
-                        "[rect=" + StringUtil.toString(rect) + "].");
+            Log.warning("Attempt to add invalid rectangle as a dirty " +
+                        "region?! [rect=" + StringUtil.toString(rect) + "].");
             Thread.dumpStack();
             return;
         }
