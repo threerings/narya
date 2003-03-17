36bcb083cad9cc42a4c36c8f37a7633c1ea55e16
Michael Bayne <mdb@threerings.net>
2003-03-17 19:21:45 +0000

    Added a constructor that takes a message bundle and a code and creates a qualified error code with them.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2308 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/server/InvocationException.java
+++ src/java/com/threerings/presents/server/InvocationException.java
@@ -1,7 +1,9 @@
 //
-// $Id: InvocationException.java,v 1.1 2002/08/14 19:07:56 mdb Exp $
+// $Id: InvocationException.java,v 1.2 2003/03/17 19:21:45 mdb Exp $
 
 package com.threerings.presents.server;
+
+import com.threerings.util.MessageBundle;
 
 /**
  * Used to report failures when executing service requests.
@@ -15,5 +17,16 @@ public class InvocationException extends Exception
     public InvocationException (String cause)
     {
         super(cause);
+    }
+
+    /**
+     * Constructs an invocation exception with the supplied cause code
+     * string and qualifying message bundle. The error code will be
+     * qualified with the message bundle (see {@link
+     * MessageBundle#qualify}).
+     */
+    public InvocationException (String bundle, String code)
+    {
+        this(MessageBundle.qualify(bundle, code));
     }
 }
