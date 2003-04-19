7772cdc1753222331a583efc4a22e26a2bc8cfd1
Michael Bayne <mdb@threerings.net>
2003-04-19 02:01:08 +0000

    Make our bounds available.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2428 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/miso/client/SceneBlock.java
+++ src/java/com/threerings/miso/client/SceneBlock.java
@@ -1,5 +1,5 @@
 //
-// $Id: SceneBlock.java,v 1.3 2003/04/18 23:18:30 mdb Exp $
+// $Id: SceneBlock.java,v 1.4 2003/04/19 02:01:08 mdb Exp $
 
 package com.threerings.miso.client;
 
@@ -77,6 +77,14 @@ public class SceneBlock
         for (int ii = 0; ii < _objects.length; ii++) {
             _objects[ii] = new SceneObject(panel, set.get(ii));
         }
+    }
+
+    /**
+     * Returns the bounds of this block, in tile coordinates.
+     */
+    public Rectangle getBounds ()
+    {
+        return _bounds;
     }
 
     /**
