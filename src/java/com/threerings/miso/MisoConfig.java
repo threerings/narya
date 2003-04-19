95b19feb5126fb847b5b220c43508e33c573b3dd
Michael Bayne <mdb@threerings.net>
2003-04-19 22:37:57 +0000

    The scene metrics are now *much* simpler.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2433 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/miso/MisoConfig.java
+++ src/java/com/threerings/miso/MisoConfig.java
@@ -1,5 +1,5 @@
 //
-// $Id: MisoConfig.java,v 1.2 2003/04/17 19:21:16 mdb Exp $
+// $Id: MisoConfig.java,v 1.3 2003/04/19 22:37:57 mdb Exp $
 
 package com.threerings.miso;
 
@@ -24,10 +24,7 @@ public class MisoConfig
         return new MisoSceneMetrics(
             config.getValue(TILE_WIDTH_KEY, DEF_TILE_WIDTH),
             config.getValue(TILE_HEIGHT_KEY, DEF_TILE_HEIGHT),
-            config.getValue(FINE_GRAN_KEY, DEF_FINE_GRAN),
-            config.getValue(SCENE_VWIDTH_KEY, DEF_SCENE_VWIDTH),
-            config.getValue(SCENE_VHEIGHT_KEY, DEF_SCENE_VHEIGHT),
-            config.getValue(SCENE_OFFSET_Y_KEY, DEF_OFFSET_Y));
+            config.getValue(FINE_GRAN_KEY, DEF_FINE_GRAN));
     }
 
     /** The config key for tile width in pixels. */
@@ -39,28 +36,8 @@ public class MisoConfig
     /** The config key for tile fine coordinate granularity. */
     protected static final String FINE_GRAN_KEY = "fine_granularity";
 
-    /** The config key for scene view width in tile count. */
-    protected static final String SCENE_VWIDTH_KEY = "scene_view_width";
-
-    /** The config key for scene view height in tile count. */
-    protected static final String SCENE_VHEIGHT_KEY = "scene_view_height";
-
-    /** The config key for scene width in tile count. */
-    protected static final String SCENE_WIDTH_KEY = "scene_width";
-
-    /** The config key for scene height in tile count. */
-    protected static final String SCENE_HEIGHT_KEY = "scene_height";
-
-    /** The config key for scene origin vertical offset in tile count. */
-    protected static final String SCENE_OFFSET_Y_KEY = "scene_offset_y";
-
     /** Default scene view parameters. */
     protected static final int DEF_TILE_WIDTH = 64;
     protected static final int DEF_TILE_HEIGHT = 48;
     protected static final int DEF_FINE_GRAN = 4;
-    protected static final int DEF_SCENE_VWIDTH = 10;
-    protected static final int DEF_SCENE_VHEIGHT = 12;
-    protected static final int DEF_SCENE_WIDTH = 22;
-    protected static final int DEF_SCENE_HEIGHT = 22;
-    protected static final int DEF_OFFSET_Y = -5;
 }
