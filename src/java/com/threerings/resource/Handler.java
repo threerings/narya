26d15d3d23178b6f08771ff5284b5a2e86163be4
Ray Greenwell <ray@threerings.net>
2003-04-24 07:26:45 +0000

    Corrected documentation: the resource set is supplied, not the specific bundle.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2450 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/resource/Handler.java
+++ src/java/com/threerings/resource/Handler.java
@@ -1,5 +1,5 @@
 //
-// $Id: Handler.java,v 1.1 2003/04/24 07:23:47 ray Exp $
+// $Id: Handler.java,v 1.2 2003/04/24 07:26:45 ray Exp $
 
 package com.threerings.resource;
 
@@ -13,13 +13,13 @@ import java.net.URLStreamHandler;
 /**
  * This class is not used directly, except by a registering ResourceManager
  * so that we can load data from the resource manager using URLs of the form
- * <code>resource://&lt;bundle&gt;/&lt;path&gt;</code>.
+ * <code>resource://&lt;resourceSet&gt;/&lt;path&gt;</code>.
  */
 public class Handler extends URLStreamHandler
 {
     /**
      * Register this class to handle "resource" urls
-     * ("resource://<i>bundlename</i>/<i>path</i>") with the specified
+     * ("resource://<i>resourceSet</i>/<i>path</i>") with the specified
      * ResourceManager.
      */
     public static void registerHandler (ResourceManager rmgr)
