7c5d0f275e6950e488fa131e014ca6703e00d1c0
Michael Bayne <mdb@threerings.net>
2003-04-28 18:12:09 +0000

    Document that little feller.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2487 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/miso/client/MisoScenePanel.java
+++ src/java/com/threerings/miso/client/MisoScenePanel.java
@@ -1,5 +1,5 @@
 //
-// $Id: MisoScenePanel.java,v 1.17 2003/04/28 18:08:57 mdb Exp $
+// $Id: MisoScenePanel.java,v 1.18 2003/04/28 18:12:09 mdb Exp $
 
 package com.threerings.miso.client;
 
@@ -1341,6 +1341,8 @@ public class MisoScenePanel extends VirtualMediaPanel
     /** A count of blocks in the process of being resolved. */
     protected int _pendingBlocks;
 
+    /** Used to avoid repaints while we don't yet have resolved all the
+     * blocks needed to render the visible view. */
     protected boolean _delayRepaint = false;
 
     /** A list of the potentially visible objects in the scene. */
