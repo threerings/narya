9a5bea62fdeb69fda4c0dc73d955ac4648e36f20
Michael Bayne <mdb@threerings.net>
2003-05-09 03:22:16 +0000

    Possibly working code to cross-compile Windows native DLL. Whee!
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@2559 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/java/com/threerings/util/unsafe/i686-Windows/com_threerings_util_unsafe_Unsafe.c
@@ -0,0 +1,52 @@
+/*
+ * $Id: com_threerings_util_unsafe_Unsafe.c,v 1.1 2003/05/09 03:22:16 mdb Exp $
+ */
+
+#include <stdio.h>
+#include <jni.h>
+#include <jvmpi.h>
+
+#include "com_threerings_util_unsafe_Unsafe.h"
+
+/* global jvmpi interface pointer */
+static JVMPI_Interface* jvmpi;
+
+JNIEXPORT void JNICALL
+Java_com_threerings_util_unsafe_Unsafe_enableGC (JNIEnv* env, jclass clazz)
+{
+    fprintf(stderr, "Reenabling GC.\n");
+    jvmpi->EnableGC();
+}
+
+JNIEXPORT void JNICALL
+Java_com_threerings_util_unsafe_Unsafe_disableGC (JNIEnv* env, jclass clazz)
+{
+    fprintf(stderr, "Disabling GC.\n");
+    jvmpi->DisableGC();
+}
+
+JNIEXPORT void JNICALL
+Java_com_threerings_util_unsafe_Unsafe_nativeSleep (
+    JNIEnv* env, jclass clazz, jint millis)
+{
+    /* not supported */
+}
+
+JNIEXPORT jboolean JNICALL
+Java_com_threerings_util_unsafe_Unsafe_init (JNIEnv* env, jclass clazz)
+{
+    JavaVM* jvm;
+
+    if ((*env)->GetJavaVM(env, &jvm) > 0) {
+        fprintf(stderr, "Failed to get JavaVM from env.\n");
+        return JNI_FALSE;
+    }
+
+    /* get jvmpi interface pointer */
+    if (((*jvm)->GetEnv(jvm, (void**)&jvmpi, JVMPI_VERSION_1)) < 0) {
+        fprintf(stderr, "Failed to get JVMPI from JavaVM.\n");
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
