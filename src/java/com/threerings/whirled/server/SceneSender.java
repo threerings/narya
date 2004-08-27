a9720fd90598ff95b27b2bf214940883b9737df7
Michael Bayne <mdb@threerings.net>
2004-08-27 02:21:06 +0000

    LGPLed the library. Hello world.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@3099 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/whirled/server/SceneSender.java
+++ src/java/com/threerings/whirled/server/SceneSender.java
@@ -1,5 +1,23 @@
 //
-// $Id: SceneSender.java,v 1.2 2002/08/20 19:38:15 mdb Exp $
+// $Id: SceneSender.java,v 1.3 2004/08/27 02:20:43 mdb Exp $
+//
+// Narya library - tools for developing networked games
+// Copyright (C) 2002-2004 Three Rings Design, Inc., All Rights Reserved
+// http://www.threerings.net/code/narya/
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package com.threerings.whirled.server;
 
@@ -13,7 +31,7 @@ import com.threerings.whirled.client.SceneReceiver;
  * client.
  *
  * <p> Generated from <code>
- * $Id: SceneSender.java,v 1.2 2002/08/20 19:38:15 mdb Exp $
+ * $Id: SceneSender.java,v 1.3 2004/08/27 02:20:43 mdb Exp $
  * </code>
  */
 public class SceneSender extends InvocationSender
--- tests/src/java/com/threerings/presents/server/TestProvider.java
+++ tests/src/java/com/threerings/presents/server/TestProvider.java
@@ -1,5 +1,23 @@
 //
-// $Id: TestProvider.java,v 1.11 2002/08/14 19:08:00 mdb Exp $
+// $Id: TestProvider.java,v 1.12 2004/08/27 02:21:04 mdb Exp $
+//
+// Narya library - tools for developing networked games
+// Copyright (C) 2002-2004 Three Rings Design, Inc., All Rights Reserved
+// http://www.threerings.net/code/narya/
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 package com.threerings.presents.server;
 
