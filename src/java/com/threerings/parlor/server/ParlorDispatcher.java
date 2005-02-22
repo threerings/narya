44f2a29f4f58f8beed401d4cacbfbca6bc1aaf01
Michael Bayne <mdb@threerings.net>
2005-02-22 18:56:52 +0000

    Regenerated with updated copyright information.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@3365 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/parlor/server/ParlorDispatcher.java
+++ src/java/com/threerings/parlor/server/ParlorDispatcher.java
@@ -2,7 +2,7 @@
 // $Id$
 //
 // Narya library - tools for developing networked games
-// Copyright (C) 2002-2004 Three Rings Design, Inc., All Rights Reserved
+// Copyright (C) 2002-2005 Three Rings Design, Inc., All Rights Reserved
 // http://www.threerings.net/code/narya/
 //
 // This library is free software; you can redistribute it and/or modify it
