008b9ad8964674d898367e87b65b5552c47eba90
Ray Greenwell <ray@threerings.net>
2005-02-26 14:04:25 +0000

    When a new default access controller is set, switch any objects that were using the old default (usually null) to use the new default.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@3369 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/server/PresentsDObjectMgr.java
+++ src/java/com/threerings/presents/server/PresentsDObjectMgr.java
@@ -79,7 +79,17 @@ public class PresentsDObjectMgr
      */
     public void setDefaultAccessController (AccessController controller)
     {
+        AccessController oldDefault = _defaultController;
         _defaultController = controller;
+
+        // switch all objects from the old default (null, usually)
+        // to the new default
+        for (Iterator itr = _objects.elements(); itr.hasNext(); ) {
+            DObject obj = (DObject) itr.next();
+            if (oldDefault == obj.getAccessController()) {
+                obj.setAccessController(controller);
+            }
+        }
     }
 
     // documentation inherited from interface
