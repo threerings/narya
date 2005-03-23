4bfbb96dfe7dcfd12917b40173f07de7e076449f
Michael Bayne <mdb@threerings.net>
2005-03-23 01:56:54 +0000

    We need to override hashCode() and equals() because the default implementations do a fucking DNS lookup on the host of the URL. The degree to which that violates expectations about the "expense" of calling hashCode() is so astronomical as to be unexpressible without complicated metaphor.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@3421 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/resource/Handler.java
+++ src/java/com/threerings/resource/Handler.java
@@ -69,6 +69,18 @@ public class Handler extends URLStreamHandler
     }
 
     // documentation inherited
+    protected int hashCode (URL url)
+    {
+        return String.valueOf(url).hashCode();
+    }
+
+    // documentation inherited
+    protected boolean equals (URL u1, URL u2)
+    {
+        return String.valueOf(u1).equals(String.valueOf(u2));
+    }
+
+    // documentation inherited
     protected URLConnection openConnection (URL url)
         throws IOException
     {
