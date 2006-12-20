51303057c8354e93d05402440ace7bfd8878236e
Michael Bayne <mdb@threerings.net>
2006-12-20 23:33:22 +0000

    Add E_ versions of our errors to support a new world order wherein error response constants start with E_ and their codes start with e..
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@4491 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/data/InvocationCodes.java
+++ src/java/com/threerings/presents/data/InvocationCodes.java
@@ -22,22 +22,28 @@
 package com.threerings.presents.data;
 
 /**
- * The invocation codes interface provides codes that are commonly used by
- * invocation service implementations. It is implemented as an interface
- * so that were an invocation service to desire to build on two or more
- * other services, it can provide a codes interface that inherits from all
+ * The invocation codes interface provides codes that are commonly used by invocation service
+ * implementations. It is implemented as an interface so that were an invocation service to desire
+ * to build on two or more other services, it can provide a codes interface that inherits from all
  * of the services that it extends.
  */
 public interface InvocationCodes
 {
-    /** An error code returned to clients when a service cannot be
-     * performed because of some internal server error that we couldn't
-     * explain in any meaningful way (things like null pointer
-     * exceptions). */
+    /** An error code returned to clients when a service cannot be performed because of some
+     * internal server error that we couldn't explain in any meaningful way (things like null
+     * pointer exceptions). */
     public static final String INTERNAL_ERROR = "m.internal_error";
 
-    /** An error code returned to clients when a service cannot be
-     * performed because the requesting client does not have the proper
-     * access. */
+    /** An error code returned to clients when a service cannot be performed because the requesting
+     * client does not have the proper access. */
     public static final String ACCESS_DENIED = "m.access_denied";
+
+    /** An error code returned to clients when a service cannot be performed because of some
+     * internal server error that we couldn't explain in any meaningful way (things like null
+     * pointer exceptions). */
+    public static final String E_INTERNAL_ERROR = "e.internal_error";
+
+    /** An error code returned to clients when a service cannot be performed because the requesting
+     * client does not have the proper access. */
+    public static final String E_ACCESS_DENIED = "e.access_denied";
 }
