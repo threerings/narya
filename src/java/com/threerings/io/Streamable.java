d3064398e9306e34c77c9f1d170f9d201133e8c1
Michael Bayne <mdb@threerings.net>
2007-02-23 19:59:18 +0000

    Widened, removed misleading comment. It's all non-transient fields, not all non-transient public fields.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@4600 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/io/Streamable.java
+++ src/java/com/threerings/io/Streamable.java
@@ -22,25 +22,22 @@
 package com.threerings.io;
 
 /**
- * Marks an object as streamable, meaning that it can be written to {@link
- * ObjectOutputStream} instances and read from {@link ObjectInputStream}
- * instances.
+ * Marks an object as streamable, meaning that it can be written to {@link ObjectOutputStream}
+ * instances and read from {@link ObjectInputStream} instances.
  *
- * <p> All non-<code>transient</code> public fields will be automatically
- * written and restored for a {@link Streamable} instance. Classes that
- * wish to stream non-public fields or customize the streaming process
- * should implement methods with the following signatures:
+ * <p> All non-<code>transient</code> fields will be automatically written and restored for a
+ * {@link Streamable} instance. Classes that wish to stream non-public fields or customize the
+ * streaming process should implement methods with the following signatures:
  *
  * <p><code>
  * public void writeObject ({@link ObjectOutputStream} out);
  * public void readObject ({@link ObjectInputStream} in);
  * </code>
  *
- * <p> They can then handle the entirety of the streaming process, or call
- * {@link ObjectOutputStream#defaultWriteObject} and {@link
- * ObjectInputStream#defaultReadObject} from within their
- * <code>writeObject</code> and <code>readObject</code> methods to perform
- * the standard streaming in addition to their customized behavior.
+ * <p> They can then handle the entirety of the streaming process, or call {@link
+ * ObjectOutputStream#defaultWriteObject} and {@link ObjectInputStream#defaultReadObject} from
+ * within their <code>writeObject</code> and <code>readObject</code> methods to perform the
+ * standard streaming in addition to their customized behavior.
  */
 public interface Streamable
 {
