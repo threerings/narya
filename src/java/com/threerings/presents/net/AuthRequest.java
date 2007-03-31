30252afc83133c1310fb76c480d1e2a4b79bbea3
Michael Bayne <mdb@threerings.net>
2007-03-31 02:35:49 +0000

    Apparently that's an IOException by the time it makes it up to us.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@4641 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/net/AuthRequest.java
+++ src/java/com/threerings/presents/net/AuthRequest.java
@@ -100,7 +100,7 @@ public class AuthRequest extends UpstreamMessage
     {
         try {
             in.defaultReadObject();
-        } catch (EOFException eofe) {
+        } catch (IOException ioe) {
             // if we fail here because the client is old, leave ourselves with a partially
             // initialized set of credentials, which the server will generally cope with by telling
             // the client it is out of date
