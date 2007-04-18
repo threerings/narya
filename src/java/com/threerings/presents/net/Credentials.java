cefe7f0788f8f4539a89e8f91059700aafebfb8f
Michael Bayne <mdb@threerings.net>
2007-04-18 01:16:57 +0000

    Widened.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@4665 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/net/Credentials.java
+++ src/java/com/threerings/presents/net/Credentials.java
@@ -25,18 +25,16 @@ import com.threerings.io.Streamable;
 import com.threerings.util.Name;
 
 /**
- * Credentials are supplied by the client implementation and sent along to
- * the server during the authentication process. To provide support for a
- * variety of authentication methods, the credentials class is meant to be
- * subclassed for the particular method (ie. password, auth digest, etc.)
- * in use in a given system.
+ * Credentials are supplied by the client implementation and sent along to the server during the
+ * authentication process. To provide support for a variety of authentication methods, the
+ * credentials class is meant to be subclassed for the particular method (ie. password, auth
+ * digest, etc.)  in use in a given system.
  *
- * <p> All credentials must provide a username as the username is used to
- * associate network connections with sessions.
+ * <p> All credentials must provide a username as the username is used to associate network
+ * connections with sessions.
  *
- * <p> All derived classes should provide a no argument constructor so
- * that they can be instantiated prior to reconstruction from a data input
- * stream.
+ * <p> All derived classes should provide a no argument constructor so that they can be
+ * instantiated prior to reconstruction from a data input stream.
  */
 public abstract class Credentials implements Streamable
 {
@@ -49,8 +47,7 @@ public abstract class Credentials implements Streamable
     }
 
     /**
-     * Constructs a blank credentials instance in preparation for
-     * unserializing from the network.
+     * Constructs a blank credentials instance in preparation for unserializing from the network.
      */
     public Credentials ()
     {
@@ -93,8 +90,8 @@ public abstract class Credentials implements Streamable
     }
 
     /**
-     * An easily extensible method via which derived classes can add to
-     * {@link #toString()}'s output.
+     * An easily extensible method via which derived classes can add to {@link #toString()}'s
+     * output.
      */
     protected void toString (StringBuilder buf)
     {
