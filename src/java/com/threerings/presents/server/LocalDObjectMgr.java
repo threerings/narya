d9f76c98fdadd4a99b3522d397e82b93f4b20512
Michael Bayne <mdb@threerings.net>
2007-05-15 00:26:27 +0000

    Some of the widening.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@4711 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/server/LocalDObjectMgr.java
+++ src/java/com/threerings/presents/server/LocalDObjectMgr.java
@@ -29,19 +29,17 @@ import com.threerings.presents.dobj.DObjectManager;
 import com.threerings.presents.dobj.Subscriber;
 
 /**
- * A special version of the distributed object manager, modified to
- * operate on the AWT thread so that it can run in a client with a GUI and
- * provide a "light" server for local operation of a normally distributed
- * application.
+ * A special version of the distributed object manager, modified to operate on the AWT thread so
+ * that it can run in a client with a GUI and provide a "light" server for local operation of a
+ * normally distributed application.
  */
 public class LocalDObjectMgr extends PresentsDObjectMgr
 {
     /**
-     * Creates a {@link DObjectManager} that posts directly to this local
-     * object manager, but first sets the source oid of all events to properly
-     * identify them with the supplied client oid. Normally this oid setting
-     * happens when an event is received on the server over the network, but in
-     * local mode we have to do it by hand.
+     * Creates a {@link DObjectManager} that posts directly to this local object manager, but first
+     * sets the source oid of all events to properly identify them with the supplied client oid.
+     * Normally this oid setting happens when an event is received on the server over the network,
+     * but in local mode we have to do it by hand.
      */
     public DObjectManager getClientDObjectMgr (final int clientOid)
     {
@@ -49,12 +47,10 @@ public class LocalDObjectMgr extends PresentsDObjectMgr
             public boolean isManager (DObject object) {
                 return LocalDObjectMgr.this.isManager(object);
             }
-            public <T extends DObject> void subscribeToObject (
-                int oid, Subscriber<T> target) {
+            public <T extends DObject> void subscribeToObject (int oid, Subscriber<T> target) {
                 LocalDObjectMgr.this.subscribeToObject(oid, target);
             }
-            public <T extends DObject> void unsubscribeFromObject (
-                int oid, Subscriber<T> target) {
+            public <T extends DObject> void unsubscribeFromObject (int oid, Subscriber<T> target) {
                 LocalDObjectMgr.this.unsubscribeFromObject(oid, target);
             }
             public void postEvent (DEvent event) {
@@ -86,10 +82,9 @@ public class LocalDObjectMgr extends PresentsDObjectMgr
     // documentation inherited
     public void postRunnable (Runnable unit)
     {
-        // we just pass this right on to the AWT event queue rather than
-        // running them through processUnit() which would basically just
-        // call run() though we lose a tiny bit of inconsequential
-        // accounting data
+        // we just pass this right on to the AWT event queue rather than running them through
+        // processUnit() which would basically just call run() though we lose a tiny bit of
+        // inconsequential accounting data
         EventQueue.invokeLater(unit);
     }
 }
