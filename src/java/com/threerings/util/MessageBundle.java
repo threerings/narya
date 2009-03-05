5bd1ef9d8cdd8d5a6b802814e29f0a57fd52f20b
Michael Bayne <mdb@threerings.net>
2009-03-05 01:22:49 +0000

    Use new isTainted and untaint methods.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@5680 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/util/MessageBundle.java
+++ src/java/com/threerings/util/MessageBundle.java
@@ -172,8 +172,8 @@ public class MessageBundle
     {
         // if this string is tainted, we don't translate it, instead we
         // simply remove the taint character and return it to the caller
-        if (key.startsWith(MessageUtil.TAINT_CHAR)) {
-            return key.substring(1);
+        if (MessageUtil.isTainted(key)) {
+            return MessageUtil.untaint(key);
         }
 
         String msg = getResourceString(key);
@@ -409,8 +409,8 @@ public class MessageBundle
             for (int i = 0; i < args.length; i++) {
                 // if the argument is tainted, do no further translation
                 // (it might contain |s or other fun stuff)
-                if (args[i].startsWith(MessageUtil.TAINT_CHAR)) {
-                    args[i] = MessageUtil.unescape(args[i].substring(1));
+                if (MessageUtil.isTainted(args[i])) {
+                    args[i] = MessageUtil.unescape(MessageUtil.untaint(args[i]));
                 } else {
                     args[i] = xlate(MessageUtil.unescape(args[i]));
                 }
