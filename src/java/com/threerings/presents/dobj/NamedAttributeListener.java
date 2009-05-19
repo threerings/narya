0f70a5c8d2da7f1d30e9832ef649ac42f888da09
Dave Hoover <dhoover@threerings.net>
2009-05-19 19:29:29 +0000

    Much like NamedSetAdapter, we had this kicking around in yohoho, also.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@5791 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/java/com/threerings/presents/dobj/NamedAttributeListener.java
@@ -0,0 +1,55 @@
+//
+// $Id$
+//
+// Narya library - tools for developing networked games
+// Copyright (C) 2002-2009 Three Rings Design, Inc., All Rights Reserved
+// http://www.threerings.net/code/narya/
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package com.threerings.presents.dobj;
+
+import com.threerings.presents.dobj.AttributeChangeListener;
+import com.threerings.presents.dobj.AttributeChangedEvent;
+
+/**
+ * A AttributeChangeListener that listens for changes with a given name and calls
+ * <code>namedAttributeChanged</code> when they occur.
+ */
+public abstract class NamedAttributeListener
+    implements AttributeChangeListener
+{
+    /**
+     * Listen for AttributeChangedEvent events with the given name.
+     */
+    public NamedAttributeListener (String name)
+    {
+        _name = name;
+    }
+
+    final public void attributeChanged (AttributeChangedEvent event)
+    {
+        if (event.getName().equals(_name)) {
+            namedAttributeChanged(event);
+        }
+    }
+
+    /**
+     * The attribute this listener is watching has changed.
+     */
+    public abstract void namedAttributeChanged (AttributeChangedEvent event);
+
+    protected final String _name;
+}
