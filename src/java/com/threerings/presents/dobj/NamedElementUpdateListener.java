9269d5b6174f86a7c8dab0f329c664d01f95341f
Dave Hoover <dhoover@threerings.net>
2009-12-15 23:38:06 +0000

    Much like NamedSetAdapter and NamedAttributeListener, I'd like to conveniently listen for ElementUpdatedEvents on specific bits on dobjs.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6003 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/java/com/threerings/presents/dobj/NamedElementUpdateListener.java
@@ -0,0 +1,52 @@
+//
+// $Id$
+//
+// Narya library - tools for developing networked games
+// Copyright (C) 2002-2009 Three Rings Design, Inc., All Rights Reserved
+// http://www.threerings.net/code/narya/
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package com.threerings.presents.dobj;
+
+/**
+ * An ElementUpdateListener that listens for changes with a given name and calls
+ * namedElementUpdated when they occur.
+ */
+public class NamedElementUpdateListener
+    implements ElementUpdateListener
+{
+    /**
+     * Listen for element updates with the given name.
+     */
+    public NamedElementUpdateListener (String name)
+    {
+        _name = name;
+    }
+
+    final public void elementUpdated (ElementUpdatedEvent event)
+    {
+        if (event.getName().equals(_name)) {
+            namedElementUpdated(event);
+        }
+    }
+
+    public void namedElementUpdated (ElementUpdatedEvent event)
+    {
+        // Override to provide functionality
+    }
+
+    protected final String _name;
+}
