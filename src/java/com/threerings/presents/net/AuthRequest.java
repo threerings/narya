bcbeb033456f453357a4acc567a2e9059e41eee8
Charlie Groves <charlie@threerings.net>
2010-10-15 07:56:13 +0000

    Only two pieces of debugging code slipped past me in that small commit
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6200 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/java/com/threerings/presents/net/AuthRequest.java
+++ src/java/com/threerings/presents/net/AuthRequest.java
@@ -102,7 +102,6 @@ public class AuthRequest extends UpstreamMessage
             // if we fail here because the client is old, leave ourselves with a partially
             // initialized set of credentials, which the server will generally cope with by telling
             // the client it is out of date
-            ioe.printStackTrace();
         }
     }
 
