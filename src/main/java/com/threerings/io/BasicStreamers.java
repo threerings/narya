4209af8963404464af849eb6749c654e743c4eff
Charlie Groves <charlie@threerings.net>
2010-12-09 21:56:04 +0000

    Unused imports
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6356 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/main/java/com/threerings/io/BasicStreamers.java
+++ src/main/java/com/threerings/io/BasicStreamers.java
@@ -21,10 +21,7 @@
 
 package com.threerings.io;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
