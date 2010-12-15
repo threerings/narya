4d163ffd7f958e77f0c6a76f082f1c92fa1bb638
Charlie Groves <charlie@threerings.net>
2010-12-15 23:46:56 +0000

    Allow generated sections to be turned off.
    If a generated section like
    
    // GENERATED BLAH START
    var s: String = "I am some awesome stuff you didn't have to write";
    // GENERATED BLAH END
    
    is replaced with
    
    // GENERATED BLAH DISABLED
    
    that generated section will no longer be merged in.
    
    This currently allows the class declaration to be customized in the actionscript
    stuff, if it needs to extend or implement something that can't be inferred from
    the Java class.  If someone's feeling frisky, they could split the streamable
    section into sections for readObject, writeObject and fields to allow for read
    and write customization.
    
    This also fixes dropping no-longer generated sections and allows for sections
    to be reordered in the source.
    
    
    
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6383 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/test/java/com/threerings/presents/tools/GeneratedSourceMergerTest.java
@@ -0,0 +1,61 @@
+package com.threerings.presents.tools;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GeneratedSourceMergerTest
+{
+    @Test
+    public void mergeNothing ()
+        throws Exception
+    {
+        new GeneratedSourceMerger().merge("", "");
+    }
+
+    @Test
+    public void mergeUpdatedSection ()
+        throws Exception
+    {
+        String modified = before + "// GENERATED VARIABLE START\n" +
+        "var s :String = 'byte';\n" +
+        "// GENERATED VARIABLE END\n" + after;
+
+        assertEquals(generated, new GeneratedSourceMerger().merge(generated, modified));
+    }
+
+    @Test
+    public void mergeMissingSection ()
+        throws Exception
+    {
+        assertEquals(section + before, new GeneratedSourceMerger().merge(generated, before));
+    }
+
+    @Test
+    public void ignoreDisabledSection ()
+        throws Exception
+    {
+        String disabled = "// GENERATED VARIABLE DISABLED\n" + before;
+        assertEquals(disabled, new GeneratedSourceMerger().merge(generated, disabled));
+    }
+
+    @Test
+    public void dropOldSection ()
+        throws Exception
+    {
+        String previous = "// GENERATED PREVIOUS START\n" +
+            "var noLongerNeeded :String = 'hi';\n" +
+            "// GENERATED PREVIOUS END\n" +
+            generated;
+        assertEquals(generated, new GeneratedSourceMerger().merge(generated, previous));
+    }
+
+    String section = "// GENERATED VARIABLE START\n" +
+     "var s :String = 'hi';\n" +
+    "// GENERATED VARIABLE END\n";
+
+    String before = "var r :int = 7;\n";
+    String after = "var t :Array = [];\n";
+    String generated = before + section + after;
+
+}
