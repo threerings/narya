0891dca5cf4a840f19f6c85a3085f2a80de7730f
Dave Hoover <dhoover@threerings.net>
2011-05-26 17:31:26 +0000

    Unused import
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6645 542714f4-19e9-0310-aa3c-eee0fc999fb1

--- src/main/java/com/threerings/presents/peer/server/PeerManager.java
+++ src/main/java/com/threerings/presents/peer/server/PeerManager.java
@@ -23,7 +23,6 @@ package com.threerings.presents.peer.server;
 
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
