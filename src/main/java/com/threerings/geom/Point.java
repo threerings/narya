a27291b430a9331eb9ac3060d9c2dca3b0f84dc6
Mike Thomas <mthomas@threerings.net>
2011-06-02 22:31:44 +0000

    It's useful to have our own friendly, simple rectangles and points independent from the various graphics systems that may or may not be available (e.g. java.awt)
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6648 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/main/java/com/threerings/geom/Point.java
@@ -0,0 +1,24 @@
+package com.threerings.geom;
+
+/**
+ * A very simple representation of the geometric concept, because sometimes we can't use awt.
+ */
+public class Point
+{
+    /** The x-coordinate. */
+    public int x;
+
+    /** The y-coordinate. */
+    public int y;
+
+    public Point ()
+    {
+        this(0, 0);
+    }
+
+    public Point (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
