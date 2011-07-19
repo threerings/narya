d01fa9523886b1a070be86ab07ba5748c8f9204e
Par Winzell <zell@threerings.net>
2011-07-19 07:00:31 +0000

    Add a Java-side stub for Hashable, just like its buddy Equalable.
    git-svn-id: svn+ssh://src.earth.threerings.net/narya/trunk@6685 542714f4-19e9-0310-aa3c-eee0fc999fb1

---
+++ src/main/java/com/threerings/util/Hashable.java
@@ -0,0 +1,12 @@
+//
+// Who - Copyright 2010-2011 Three Rings Design, Inc.
+
+package com.threerings.util;
+
+/**
+ * This exists solely for classes with generated ActionScript counterparts that need to implement
+ * the Hashable.as interface
+ */
+public interface Hashable
+{
+}
