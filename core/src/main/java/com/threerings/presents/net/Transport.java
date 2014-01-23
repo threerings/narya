8f94d9c3291a3976cf18ddf9ac512043479d65e2
Michael Bayne <mdb@samskivert.com>
2014-01-23 14:24:26 -0800

    Cast differently.

--- core/src/main/java/com/threerings/presents/net/Transport.java
+++ core/src/main/java/com/threerings/presents/net/Transport.java
@@ -145,8 +145,8 @@ public class Transport
         if (_unordered == null) {
             int length = Type.values().length;
             _unordered = new Transport[length];
-            @SuppressWarnings({ "unchecked", "rawtypes" }) HashIntMap<Transport>[] ordered =
-                new HashIntMap[length];
+            @SuppressWarnings({ "unchecked" }) HashIntMap<Transport>[] ordered =
+                (HashIntMap<Transport>[])new HashIntMap<?>[length];
             _ordered = ordered;
         }
 
