32b4bb32608e3154dccc0390a3143d91848b2a68
Ray J. Greenwell <ray@bogocorp.com>
2026-07-31 21:10:52 -0700

    Just make AccessController default method implementations.
    Nix the composition methods. Fancy things can be done on your project's
    subclass of AccessController.

--- core/src/main/java/com/threerings/presents/dobj/AccessController.java
+++ core/src/main/java/com/threerings/presents/dobj/AccessController.java
@@ -5,8 +5,6 @@
 
 package com.threerings.presents.dobj;
 
-import java.util.function.Predicate;
-
 /**
  * Used to validate distributed object subscription requests and event
  * dispatches.
@@ -16,63 +14,21 @@ import java.util.function.Predicate;
 public interface AccessController
 {
     /**
-     * A default access controller implementation that allows for no subscribing.
-     */
-    public static class Default
-        implements AccessController
-    {
-        public boolean allowSubscribe (DObject object, Subscriber<?> subscriber) {
-            return false;
-        }
-
-        public boolean allowDispatch (DObject object, DEvent event)
-        {
-            return event.getSourceOid() == -1;
-        }
-    }
-
-    /**
      * Should return true if the supplied subscriber is allowed to
      * subscribe to the specified object.
      */
-    boolean allowSubscribe (DObject object, Subscriber<?> subscriber);
+    default boolean allowSubscribe (DObject object, Subscriber<?> subscriber)
+    {
+        return false;
+    }
 
     /**
      * Should return true if the supplied event is legal for dispatch on
      * the specified distributed object.
      */
-    boolean allowDispatch (DObject object, DEvent event);
-
-    /**
-     * Compose a new AccessController that is like this one but ALSO allows subscribers
-     * that satisfy the predicate.
-     */
-    default AccessController orSubscribe (Predicate<Subscriber<?>> canSubscribe) {
-        final AccessController ac = this;
-        return new AccessController() {
-            public boolean allowSubscribe (DObject object, Subscriber<?> subscriber) {
-                return canSubscribe.test(subscriber) || ac.allowSubscribe(object, subscriber);
-            }
-            public boolean allowDispatch (DObject object, DEvent event) {
-                return ac.allowDispatch(object, event);
-            }
-        };
-    }
-
-    /**
-     * Compose a new AccessController that is like this one but ALSO allows event dispatches
-     * that satisfy the predicate.
-     */
-    default AccessController orDispatch (Predicate<DEvent> canDispatch) {
-        final AccessController ac = this;
-        return new AccessController() {
-            public boolean allowSubscribe (DObject object, Subscriber<?> subscriber) {
-                return ac.allowSubscribe(object, subscriber);
-            }
-            public boolean allowDispatch (DObject object, DEvent event) {
-                return canDispatch.test(event) || ac.allowDispatch(object, event);
-            }
-        };
+    default boolean allowDispatch (DObject object, DEvent event)
+    {
+        return event.getSourceOid() == -1; // most objects only permit the server to send events
     }
 }
 
